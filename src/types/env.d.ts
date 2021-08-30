91dc7a2cbdf4851a28a9cc45f4b47de82996dcfe
y-iwamoto <klc.oka.iwa.tsu.gmiw@gmail.com>
2021-08-30 16:08:00 +0900

    firebase設定と処理周り実装

---
+++ src/types/env.d.ts
@@ -0,0 +1,13 @@
+declare module '@env' {
+  export const FIREBASE_API_KEY_DEV: string;
+  export const FIREBASE_AUTH_DOMAIN_DEV: string;
+  export const FIREBASE_PROJECT_ID_DEV: string;
+
+  export const FIREBASE_STRORAGE_BUCKET_DEV: string;
+  export const FIREBASE_MESSAGING_SENDER_ID_DEV: string;
+  export const FIREBASE_APP_ID: string;
+
+  export const GOOGLE_ANDROID_CLIENT_ID_DEV: string;
+  export const GOOGLE_IOS_CLIENT_ID_DEV: string;
+  export const FACEBOOK_APP_ID_DEV: string;
+}
