22e73d161ecb0833ca18d393b9c948eeb7db5644
y-iwamoto <klc.oka.iwa.tsu.gmiw@gmail.com>
2021-09-01 11:30:30 +0900

    パンくず作成

---
+++ src/components/molecules/Breadcrumb.tsx
@@ -0,0 +1,55 @@
+
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Crumb } from '../atoms/Tab/Crumb';
+
+const HEIGHT = 23;
+
+type Props = {
+    flowDepth: number;
+    entities: Array<string>;
+    borderRadius: number;
+    isTouchable: boolean;
+    height: number;
+}
+
+export const Breadcrumb: React.FC<Props> = ({ flowDepth, entities, borderRadius, height}: Props) => {
+  const firstCrumbStyle = [{ borderTopLeftRadius: borderRadius - 1, borderBottomLeftRadius: borderRadius - 1 }];
+  const lastCrumbStyle = [{ borderTopRightRadius: borderRadius - 1, borderBottomRightRadius: borderRadius - 1 }];
+  const triangleTailStyle = height ? { borderTopWidth: height / 2.0, borderBottomWidth: height / 2.0, borderLeftWidth: height / 2.0 } : {};
+  const triangleHeadStyle = height ? { borderTopWidth: height / 2.0, borderBottomWidth: height / 2.0, borderLeftWidth: height / 2.0 } : {};
+
+  return (
+    <View
+      style={[styles.crumbsContainerStyle, borderRadius ? { borderRadius } : {}]}
+      removeClippedSubviews={false}
+    >
+      {
+        entities.map((item, index) => {
+          return (
+            <Crumb
+              key={index}
+              index={index}
+              isCrumbActive={flowDepth === index}
+              text={item}
+              firstCrumbStyle={index === 0 ? firstCrumbStyle : null}
+              lastCrumbStyle={index === entities.length - 1 ? lastCrumbStyle : null}
+              height={height ? height : HEIGHT}
+              triangleTailStyle={triangleTailStyle}
+              triangleHeadStyle={triangleHeadStyle}
+            />
+          );
+        })
+      }
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  crumbsContainerStyle: {
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: '#d6d6d6',
+    justifyContent: 'space-between',
+  },
+});
---
+++ src/components/organisms/BreadcrumbSection.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Breadcrumb } from '../molecules/Breadcrumb';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+
+
+export const BreadcrumbSection: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Breadcrumb
+        entities={['会員登録', '家計簿項目登録', '利用開始']}
+        isTouchable={false}
+        flowDepth={0}
+        height={30}
+        borderRadius={5}
+      />
+
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffff',
+    height: hp('8%'),
+    marginTop: hp('2%'),
+    marginHorizontal: wp('2%'),
+    paddingHorizontal: wp('5%'),
+    justifyContent: 'center',
+  }
+
+});
